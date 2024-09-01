8e2425dde45233e9f916d3aabf0acd891420ff74
OscarObando <osobando@estudiantec.cr>
2024-08-31 23:00:02 -0600

    Mensaje epico

---
+++ proyecto1/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+//Importo la cosa para poder usar los componentes que puse ahí
+import { Button,TextField } from './pruebas.js';
+
+function App() {
+  return (
+    <div>
+      <h1>Login Épico</h1>
+      <TextField />
+      <br />
+      <TextField />
+      <br />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
