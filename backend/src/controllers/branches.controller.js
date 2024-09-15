614b064db13b6283d7e0289a49e310acf496f14d
RaylTzy <kakashicc13@gmail.com>
2024-09-14 23:30:39 -0600

    rutas para las ramas

---
+++ backend/src/controllers/branches.controller.js
@@ -0,0 +1,17 @@
+import {couchClient} from '../db.js'
+
+export const createBranches = async (req, res) => {
+    console.log(req.body);
+    //var async = require('async');
+    //const fs = require('fs');
+    await couchClient.insert(req.body);
+    res.json("hola");
+}
+
+export const getBranches = async (req, res) => {
+    const {repositoryId} = req.query;
+    const query = { selector: { _id : repositoryId } }
+    const branches = (await couchClient.find(query));
+    res.json(branches.docs);
+    console.log(branches.docs);
+}
